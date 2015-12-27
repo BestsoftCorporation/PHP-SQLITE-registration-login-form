be715f82ba69662ba379940e3edc3454c77142ec
Marko Stojkovic <software.developer@hotmail.rs>
2015-12-27 18:12:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Button States</h2>
+  <button type="button" href="register.php" class="btn btn-primary">Sign Up</button>
+  <button type="button" href="login.php" class="btn btn-primary active">Log in</button>
+</div>
+
+</body>
+</html>
