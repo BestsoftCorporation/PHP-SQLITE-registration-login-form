35bcd3cfc39b1d8f74d3bd41f47d7743ea4057f6
Marko Stojkovic <software.developer@hotmail.rs>
2015-12-27 18:12:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Log In/Sign Up Form Example in PHP and SQLITE Database</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
