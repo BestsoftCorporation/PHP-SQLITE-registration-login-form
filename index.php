479c200bee2627b4e0387e45a49f8c79c00aaa49
Marko Stojkovic <software.developer@hotmail.rs>
2015-12-27 18:17:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
 
 <div class="container">
-  <h2>Button States</h2>
+  <h2>Log In/Sign Up Form Example in PHP and SQLITE Database</h2>
   <button type="button" href="register.php" class="btn btn-primary">Sign Up</button>
   <button type="button" href="login.php" class="btn btn-primary active">Log in</button>
 </div>
